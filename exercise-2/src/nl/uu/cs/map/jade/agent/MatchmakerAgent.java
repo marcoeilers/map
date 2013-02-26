d18efa90a136dfd3af36e3320f4830f52068137d
Robert Schmidtke <ro.schmidtke@gmail.com>
2013-02-26 18:54:40 +0100

    added matchmaking

--- exercise-2/src/nl/uu/cs/map/jade/agent/MatchmakerAgent.java
+++ exercise-2/src/nl/uu/cs/map/jade/agent/MatchmakerAgent.java
@@ -1,10 +1,37 @@
 package nl.uu.cs.map.jade.agent;
 
+import jade.core.AID;
 import jade.core.Agent;
+
+import java.util.List;
+
+import nl.uu.cs.map.jade.ItemDB;
+import nl.uu.cs.map.jade.ItemDescriptor;
 
 public class MatchmakerAgent extends Agent {
 
 	private static final long serialVersionUID = 8758414317735123415L;
+
+	/**
+	 * Result of a matchmaking request containing the specified item and one
+	 * list of buyers and sellers each.
+	 * 
+	 * @author robert
+	 * 
+	 */
+	public static class MatchMakingResult {
+		public ItemDescriptor item;
+		public List<AID> buyers, sellers;
+
+		public MatchMakingResult(ItemDescriptor item, List<AID> buyers,
+				List<AID> sellers) {
+			this.item = item;
+			this.buyers = buyers;
+			this.sellers = sellers;
+		}
+	}
+
+	private ItemDB itemDB;
 
 	@Override
 	protected void setup() {
@@ -14,7 +41,20 @@ public class MatchmakerAgent extends Agent {
 		Object[] args = getArguments();
 		for (Object arg : args)
 			System.out.println(arg);
-		super.setup();
+
+		itemDB = ItemDB.getInstance();
+	}
+
+	/**
+	 * Returns the matchmaking result that contains buyers and sellers for the
+	 * specified item.
+	 * 
+	 * @param item
+	 * @return
+	 */
+	private MatchMakingResult makeMatch(ItemDescriptor item) {
+		return new MatchMakingResult(item, itemDB.getBuyers(item),
+				itemDB.getSellers(item));
 	}
 
 }
