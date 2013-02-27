e657dee9d8ceedb81daaf95518f4bb7a8d5f0bf3
Robert Schmidtke <ro.schmidtke@gmail.com>
2013-02-27 21:58:52 +0100

    passing init file

--- exercise-2/run.sh
+++ exercise-2/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -cp lib/jade.jar:bin/:. jade.Boot -gui -host localhost -agents "matchmaker:nl.uu.cs.map.jade.agent.MatchmakerAgent(arg1,arg2)"
+java -cp lib/jade.jar:bin/:. jade.Boot -gui -host localhost -agents "matchmaker:nl.uu.cs.map.jade.agent.TraderAgent(resource/trader.properties)"
