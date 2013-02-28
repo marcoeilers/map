154e24fd0a83c1b77986bf720b05b557505cff74
Robert Schmidtke <ro.schmidtke@gmail.com>
2013-02-28 11:17:54 +0100

    added dedicated buyer and seller

--- exercise-2/run.sh
+++ exercise-2/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -cp lib/jade.jar:bin/:. jade.Boot -gui -host localhost -agents "matchmaker:nl.uu.cs.map.jade.agent.MatchmakerAgent;trader:nl.uu.cs.map.jade.agent.TraderAgent(resource/trader.properties)"
+java -cp lib/jade.jar:bin/:. jade.Boot -gui -host localhost -agents "matchmaker:nl.uu.cs.map.jade.agent.MatchmakerAgent;trader:nl.uu.cs.map.jade.agent.TraderAgent(resource/trader.properties);buyer:nl.uu.cs.map.jade.agent.TraderAgent(resource/buyer.properties);seller:nl.uu.cs.map.jade.agent.TraderAgent(resource/seller.properties)"
