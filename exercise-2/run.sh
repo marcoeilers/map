545d965f3f114450af64aecb0400c59d9b47525a
Robert Schmidtke <ro.schmidtke@gmail.com>
2013-02-28 12:20:14 +0100

    removing generated txt files

--- exercise-2/run.sh
+++ exercise-2/run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 java -cp lib/jade.jar:bin/:. jade.Boot -gui -host localhost -agents "matchmaker:nl.uu.cs.map.jade.agent.MatchmakerAgent;trader:nl.uu.cs.map.jade.agent.TraderAgent(resource/trader.properties);buyer:nl.uu.cs.map.jade.agent.TraderAgent(resource/buyer.properties);seller:nl.uu.cs.map.jade.agent.TraderAgent(resource/seller.properties)"
+rm APDescription.txt
+rm MTPs-Main-Container.txt
